b8672ee656f2d56cf6c25e2ac94f2fa7add90b66
mmellam <28260088+mmellam@users.noreply.github.com>
2021-06-27 10:49:17 +0200

    Initial React Router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
+import './components/NavBar/NavBar.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+    <Router>
+      <div className='App'>
+        <div className='navbar'>
+          <p>
+              <Link to='/'>Jammming</Link>
+          </p>
+          <nav className='navigation'>
+              <ul>
+                  <li>
+                      <Link to='/1click'>1click playlist</Link>
+                  </li>
+                  <li><a>Magic Playlist</a>
+                      <ul className='dropdown'>
+                          <li>
+                              <Link to='/magic-playlist-tracks'>For Tracks</Link>
+                          </li>
+                          <li>
+                              <Link to='/magic-playlist-artists'>For Artists</Link>
+                          </li>
+                      </ul>    
+                  </li>
+                  <li><button>Connect to your Spotify Account</button></li>
+              </ul>
+          </nav>
+          
+          <Switch>
+              <Route path='/1click'>
+                  <Users />
+              </Route>
+              <Route path='/magic-playlist-tracks'>
+                  <Home />
+              </Route>
+              <Route path='/magic-playlist-artists'>
+                  
+              </Route>
+              <Route path='/'>
+                  <About />
+              </Route>
+          </Switch>
+          
+        </div>
+      </div>  
+    </Router>
   );
+}
+
+function Home() {
+  return (
+      <div>
+          <h2>Home</h2>
+      </div>
+  )
+  ;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
