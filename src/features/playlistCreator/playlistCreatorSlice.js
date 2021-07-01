3a670c668ef72858adf92cea9497affdf234dd18
mmellam <28260088+mmellam@users.noreply.github.com>
2021-07-01 13:22:22 +0200

    Move save playlist function to util/spotify

--- src/features/playlistCreator/playlistCreatorSlice.js
+++ src/features/playlistCreator/playlistCreatorSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-
+import { savePlaylistToSpotify } from '../../util/spotify';
 
 // searches the entered search term in the Spotify API
 export const search = createAsyncThunk(
@@ -20,35 +20,7 @@ export const search = createAsyncThunk(
 export const savePlaylist = createAsyncThunk(
     'playlistCreator/savePlaylist',
     async (playlistToCreate) => {
-
-        
-        const accessToken = window.sessionStorage.accessToken;
-        const headers = { Authorization: `Bearer ${accessToken}` };
-
-        const idResponse = await fetch(`https://api.spotify.com/v1/me`, {
-            headers: headers
-        });
-        const idResponseJSON = await idResponse.json();
-        const userId = idResponseJSON.id;
-
-        const createPlaylistResponse = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-            headers: headers,
-            method: 'POST',
-            body: JSON.stringify({ name: playlistToCreate.playlistName })
-        });
-        const createPlaylistResponseJSON = await createPlaylistResponse.json();
-        const playlistId = createPlaylistResponseJSON.id;
-
-        const addTracksResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-            headers: { 
-                Authorization: `Bearer ${accessToken}`,
-                'Content-Type': 'application/json' },
-            method: 'POST',
-            body: JSON.stringify({ uris: playlistToCreate.trackUris })
-        });
-        const addTracksResponseJSON = await addTracksResponse.json();
-        //console.log(addTracksResponseJSON);
-        return addTracksResponseJSON;
+        savePlaylistToSpotify(playlistToCreate);
     }
 );
 
--- src/util/spotify.js
+++ src/util/spotify.js
@@ -27,8 +27,34 @@ const onClickGetAccessToken = () => {
     }
 };
 
-const savePlaylistToSpotify = () => {
+const savePlaylistToSpotify = async (playlistToCreate) => {
+    const accessToken = window.sessionStorage.accessToken;
+    const headers = { Authorization: `Bearer ${accessToken}` };
 
+    const idResponse = await fetch(`https://api.spotify.com/v1/me`, {
+        headers: headers
+    });
+    const idResponseJSON = await idResponse.json();
+    const userId = idResponseJSON.id;
+
+    const createPlaylistResponse = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+        headers: headers,
+        method: 'POST',
+        body: JSON.stringify({ name: playlistToCreate.playlistName })
+    });
+    const createPlaylistResponseJSON = await createPlaylistResponse.json();
+    const playlistId = createPlaylistResponseJSON.id;
+
+    const addTracksResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+        headers: { 
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify({ uris: playlistToCreate.trackUris })
+    });
+    const addTracksResponseJSON = await addTracksResponse.json();
+    console.log(addTracksResponseJSON);
+    return addTracksResponseJSON;
 }
 
 export { onClickGetAccessToken, savePlaylistToSpotify };
