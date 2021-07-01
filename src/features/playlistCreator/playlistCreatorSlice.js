ff76e1ed109e8759bf181a936371bad1f2cda84b
mmellam <28260088+mmellam@users.noreply.github.com>
2021-07-01 09:55:07 +0200

    Store search results for playlist creator in slice state

--- src/features/playlistCreator/playlistCreatorSlice.js
+++ src/features/playlistCreator/playlistCreatorSlice.js
@@ -1,15 +1,59 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 
-
+// searches the entered search term in the Spotify API
+export const search = createAsyncThunk(
+    'playlistCreator/search',
+    async (searchTerm) => {
+        const accessToken = window.sessionStorage.accessToken;
+        console.log(accessToken)
+        const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        const responseJSON = await response.json();
+        return responseJSON;
+    }
+);
 
 const playlistCreatorSlice = createSlice({
     name: 'playlistCreator',
     initialState: {
-
+        searchResults: [],
+        isLoadingResults: false,
+        failedToLoadResults: false,
+        newPlaylist: []
     },
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [search.pending]: (state) => {
+            state.isLoadingResults = true;
+            state.failedToLoadResults = false;
+        },
+        [search.fulfilled]: (state, action) => {
+            state.isLoadingResults = false;
+            state.failedToLoadResults = false;
+            const searchResults = action.payload.tracks.items.map(track => {
+                return {
+                    id: track.id,
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    uri: track.uri
+                }
+            });
+            state.searchResults = [...searchResults];
+            console.log(state.searchResults);
+        },
+        [search.rejected]: (state,action) => {
+            state.isLoadingResults = false;
+            state.failedToLoadResults = true;
+        }
+    }
     
-})
+});
+
+export const selectSearchResults = (state) => state.playlistCreator.searchResults;
 
 export default playlistCreatorSlice.reducer;
--- src/util/spotify.js
+++ src/util/spotify.js
@@ -7,16 +7,13 @@ const onClickGetAccessToken = () => {
     if (accessToken) {
         return accessToken;
     }
-
     const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-
     // get auth token from Spotify auth endpoint
     if (!accessTokenMatch || !expiresInMatch) {
         const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
         window.location = accessUrl;
     }
-
     // check if access token is provided in URL
     if (accessTokenMatch && expiresInMatch) {
         accessToken = accessTokenMatch[1];
@@ -25,13 +22,10 @@ const onClickGetAccessToken = () => {
         // clear the access token and URL parameters
         window.setTimeout(() => accessToken = '', expiresIn * 1000);
         window.history.pushState('Access Token', null, '/');
+        window.sessionStorage.accessToken = accessToken;
         console.log(accessToken);
         return accessToken;
     }
 };
 
-const search = () => {
-
-}
-
-export { onClickGetAccessToken, search };
+export { onClickGetAccessToken };
